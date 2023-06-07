9ca7111dadf39069bf765446739f07dc482ae523
Evan Griffiths <evan.griffiths@finbourne.com>
2023-06-07 10:21:20 +0100

    PLATAPP-631: Moved extensions tests into main SDK + Referencing Moq for ApiFactoryBuilderTest

---
+++ sdk/Finbourne.Notifications.Sdk.IntegrationTests/IntegrationTestApiFactoryBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Finbourne.Notifications.Sdk.Extensions;
+
+namespace Finbourne.Notifications.Sdk.IntegrationTests
+{
+    public class IntegrationTestApiFactoryBuilder
+    {
+        public static IApiFactory CreateApiFactory(string secretsFile)
+        {
+            return File.Exists(secretsFile)
+                ? ApiFactoryBuilder.Build(secretsFile)
+                : ApiFactoryBuilder.Build(null);
+        }
+
+        public static ApiConfiguration CreateApiConfiguration(string secretsFile)
+        {
+            return File.Exists(secretsFile)
+                ? ApiConfigurationBuilder.Build(secretsFile)
+                : ApiConfigurationBuilder.Build(null);
+        }
+    }
+}
---
+++ sdk/Finbourne.Notifications.Sdk.IntegrationTests/TokenProviderConfigurationTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using Finbourne.Notifications.Sdk.Extensions;
+
+namespace Finbourne.Notifications.Sdk.IntegrationTests
+{
+    [TestFixture]
+    public class TokenProviderConfigurationTest
+    {
+        private static readonly Lazy<ApiConfiguration> ApiConfig =
+            new Lazy<ApiConfiguration>(() => ApiConfigurationBuilder.Build("secrets.json"));
+
+        //Test requires [assembly: InternalsVisibleTo("Finbourne.Notifications.Sdk.Tests")] in ClientCredentialsFlowTokenProvider
+        [Test]
+        public void Construct_AccessToken_NonNull()
+        {
+            ITokenProvider tokenProvider;
+            if (ApiConfig.Value.MissingSecretVariables)
+            {
+                tokenProvider = new PersonalAccessTokenProvider(ApiConfig.Value.PersonalAccessToken);
+            }
+            else 
+            {
+                tokenProvider = new ClientCredentialsFlowTokenProvider(ApiConfigurationBuilder.Build("secrets.json")); 
+            }
+
+            var config = new TokenProviderConfiguration(tokenProvider);
+            Assert.IsNotNull(config.AccessToken);
+        }
+    }
+}
