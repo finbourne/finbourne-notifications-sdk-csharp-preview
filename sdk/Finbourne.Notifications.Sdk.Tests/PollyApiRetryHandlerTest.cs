5f133713077e352fdb6602a6033c8f6a06ae426a
Evan Griffiths <evan.griffiths@finbourne.com>
2023-06-07 19:00:53 +0100

    PLATAPP-631: Fixed PollyApiRetryHandlerTests

--- sdk/Finbourne.Notifications.Sdk.Tests/PollyApiRetryHandlerTest.cs
+++ sdk/Finbourne.Notifications.Sdk.Tests/PollyApiRetryHandlerTest.cs
@@ -1,56 +1,58 @@
-// using Finbourne.Notifications.Sdk.Extensions;
-// using NUnit.Framework;
-// using Polly;
-// using RestSharp;
-//
-// namespace Finbourne.Notifications.Sdk.Tests
-// {
-//     [TestFixture]
-//     public class PollyApiRetryHandlerTest
-//     {
-//         [Test]
-//         public void CreateApiFactory_WhenRetryPolicyIsNull_AssignsDefaultRetryPolicy()
-//         {
-//             RetryConfiguration.RetryPolicy = null;
-//
-//             new ApiFactory(new Client.Configuration());
-//
-//             Assert.That(RetryConfiguration.RetryPolicy, Is.Not.Null);
-//             Assert.That(RetryConfiguration.RetryPolicy, Is.EqualTo(PollyApiRetryHandler.DefaultRetryPolicyWithFallback));
-//         }
-//
-//         [Test]
-//         public void CreateApiFactory_WhenRetryPolicyIsAlreadyAssigned_ExistingRetryPolicyIsUsed()
-//         {
-//             var testPolicy = Policy.HandleResult<RestResponse>(response => true).Retry();
-//
-//             RetryConfiguration.RetryPolicy = testPolicy;
-//             var newFactory = new ApiFactory(new Client.Configuration());
-//
-//             Assert.That(RetryConfiguration.RetryPolicy, Is.EqualTo(testPolicy));
-//         }
-//
-//         [Test]
-//         public void CreateApiFactory_WhenRetryPolicyIsNull_AssignsDefaultAsyncRetryPolicy()
-//         {
-//             RetryConfiguration.AsyncRetryPolicy = null;
-//
-//             new ApiFactory(new Client.Configuration());
-//
-//             Assert.That(
-//                 RetryConfiguration.AsyncRetryPolicy,
-//                 Is.EqualTo(PollyApiRetryHandler.DefaultRetryPolicyWithFallbackAsync));
-//         }
-//
-//         [Test]
-//         public void CreateApiFactory_WhenRetryPolicyIsAlreadyAssigned_ExistingAsyncRetryPolicyIsUsed()
-//         {
-//             var testPolicy = Policy.HandleResult<RestResponse>(response => true).RetryAsync();
-//
-//             RetryConfiguration.AsyncRetryPolicy = testPolicy;
-//             var newFactory = new ApiFactory(new Client.Configuration());
-//
-//             Assert.That(RetryConfiguration.AsyncRetryPolicy, Is.EqualTo(testPolicy));
-//         }
-//     }
-// }
+using Finbourne.Notifications.Sdk.Extensions;
+using NUnit.Framework;
+using Polly;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Finbourne.Notifications.Sdk.Client;
+
+namespace Finbourne.Notifications.Sdk.Tests
+{
+    [TestFixture]
+    public class PollyApiRetryHandlerTest
+    {
+        [Test]
+        public void CreateApiFactory_WhenRetryPolicyIsNull_AssignsDefaultRetryPolicy()
+        {
+            RetryConfiguration.RetryPolicy = null;
+
+            new ApiFactory(new Client.Configuration());
+
+            Assert.That(RetryConfiguration.RetryPolicy, Is.Not.Null);
+            Assert.That(RetryConfiguration.RetryPolicy, Is.EqualTo(PollyApiRetryHandler.DefaultRetryPolicyWithFallback));
+        }
+
+        [Test]
+        public void CreateApiFactory_WhenRetryPolicyIsAlreadyAssigned_ExistingRetryPolicyIsUsed()
+        {
+            var testPolicy = Policy.HandleResult<HttpResponseMessage>(response => true).Retry();
+
+            RetryConfiguration.RetryPolicy = testPolicy;
+            var newFactory = new ApiFactory(new Client.Configuration());
+
+            Assert.That(RetryConfiguration.RetryPolicy, Is.EqualTo(testPolicy));
+        }
+
+        [Test]
+        public void CreateApiFactory_WhenRetryPolicyIsNull_AssignsDefaultAsyncRetryPolicy()
+        {
+            RetryConfiguration.AsyncRetryPolicy = null;
+
+            new ApiFactory(new Client.Configuration());
+
+            Assert.That(
+                RetryConfiguration.AsyncRetryPolicy,
+                Is.EqualTo(PollyApiRetryHandler.DefaultRetryPolicyWithFallbackAsync));
+        }
+
+        [Test]
+        public void CreateApiFactory_WhenRetryPolicyIsAlreadyAssigned_ExistingAsyncRetryPolicyIsUsed()
+        {
+            var testPolicy = Policy.HandleResult<HttpResponseMessage>(response => true).RetryAsync();
+
+            RetryConfiguration.AsyncRetryPolicy = testPolicy;
+            var newFactory = new ApiFactory(new Client.Configuration());
+
+            Assert.That(RetryConfiguration.AsyncRetryPolicy, Is.EqualTo(testPolicy));
+        }
+    }
+}
