054e82a92b311425a3d3744fd5fe27dbbb33b846
Concourse <concourse@finbourne.com>
2023-07-07 10:43:40 +0000

    Update SDK 0.1.775

--- sdk/Finbourne.Notifications.Sdk/Client/Configuration.cs
+++ sdk/Finbourne.Notifications.Sdk/Client/Configuration.cs
@@ -29,7 +29,7 @@ namespace Finbourne.Notifications.Sdk.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "0.1.774-2";
+        public const string Version = "0.1.775";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
@@ -57,7 +57,7 @@ namespace Finbourne.Notifications.Sdk.Client
             if (status == 0)
             {
                 return new ApiException(status,
-                    string.Format("Error calling {0}: {1}", methodName, response.ErrorText), response.ErrorText);
+                    string.Format("Error calling {0}: {1}\n{2}", methodName, response.ErrorText, response.RawContent), response.ErrorText);
             }
             return null;
         };
@@ -111,7 +111,7 @@ namespace Finbourne.Notifications.Sdk.Client
         public Configuration()
         {
             Proxy = null;
-            UserAgent = WebUtility.UrlEncode("OpenAPI-Generator/0.1.774-2/csharp");
+            UserAgent = WebUtility.UrlEncode("OpenAPI-Generator/0.1.775/csharp");
             BasePath = "https://www.lusid.com/notification";
             DefaultHeaders = new ConcurrentDictionary<string, string>();
             ApiKey = new ConcurrentDictionary<string, string>();
@@ -518,8 +518,8 @@ namespace Finbourne.Notifications.Sdk.Client
             string report = "C# SDK (Finbourne.Notifications.Sdk) Debug Report:\n";
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
-            report += "    Version of the API: 0.1.774-2\n";
-            report += "    SDK Package Version: 0.1.774-2\n";
+            report += "    Version of the API: 0.1.775\n";
+            report += "    SDK Package Version: 0.1.775\n";
 
             return report;
         }
--- sdk/docs/CreateSubscription.md
+++ sdk/docs/CreateSubscription.md
@@ -10,7 +10,7 @@ Name | Type | Description | Notes
 **Description** | **string** | The summary of the services provided by the subscription | [optional] 
 **Status** | **string** | The current status of the subscription. Possible values are: Active, Inactive | 
 **MatchingPattern** | [**MatchingPattern**](MatchingPattern.md) |  | 
-**UseAsAuth** | **string** | The user to authenticate the subscription | [optional] 
+**UseAsAuth** | **string** | Id of user associated with subscription. All events associated with   the subscription will use this user to check entitlements against   the resource to send a notification. Can be null, in which case   we&#39;ll default to that of the user making this request | [optional] 
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
 
