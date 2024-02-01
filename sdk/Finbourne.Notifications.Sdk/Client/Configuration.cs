8338e54c8a088a2b542fc93eadd558c3829948c6
Concourse <concourse@finbourne.com>
2024-02-01 09:51:16 +0000

    Update SDK 0.1.909

--- sdk/Finbourne.Notifications.Sdk/Client/Configuration.cs
+++ sdk/Finbourne.Notifications.Sdk/Client/Configuration.cs
@@ -29,7 +29,7 @@ namespace Finbourne.Notifications.Sdk.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "0.1.906";
+        public const string Version = "0.1.909";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
@@ -111,7 +111,7 @@ namespace Finbourne.Notifications.Sdk.Client
         public Configuration()
         {
             Proxy = null;
-            UserAgent = WebUtility.UrlEncode("OpenAPI-Generator/0.1.906/csharp");
+            UserAgent = WebUtility.UrlEncode("OpenAPI-Generator/0.1.909/csharp");
             BasePath = "https://www.lusid.com/notification";
             DefaultHeaders = new ConcurrentDictionary<string, string>();
             ApiKey = new ConcurrentDictionary<string, string>();
@@ -518,8 +518,8 @@ namespace Finbourne.Notifications.Sdk.Client
             string report = "C# SDK (Finbourne.Notifications.Sdk) Debug Report:\n";
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
-            report += "    Version of the API: 0.1.906\n";
-            report += "    SDK Package Version: 0.1.906\n";
+            report += "    Version of the API: 0.1.909\n";
+            report += "    SDK Package Version: 0.1.909\n";
 
             return report;
         }
